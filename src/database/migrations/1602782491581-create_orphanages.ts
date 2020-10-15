c6b75844f7fb4a079416b79cbd2580663ba266f3
Julio Gonçalves <julio.goncalves@multiway.com.br>
2020-10-15 14:29:32 -0300

    first commit

---
+++ src/database/migrations/1602782491581-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602782491581 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
