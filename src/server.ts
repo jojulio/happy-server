bada80fd13834ab2bc0acd60494f8a32295dae49
Julio Gonçalves <julio.goncalves@multiway.com.br>
2020-10-16 14:20:55 -0300

    create model orphanages

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express, { response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
 import './database/connection';
 
 const app = express();
@@ -6,6 +9,29 @@ app.use(express.json());
 
 app.listen(3333);
 
-app.get('/users', (request, response) => {
+app.post('/orphanages', async (request, response) => {
+	const {
+		name, 
+		latitude, 
+		longitude, 
+		about, 
+		instructions, 
+		opening_hours,
+		open_on_weekends,
+	} = request.body;
+
+	const orphanagesRepository = getRepository(Orphanage);
+	const orphanage = orphanagesRepository.create({
+		name,
+		latitude,
+		longitude,
+		about,
+		instructions,
+		opening_hours,
+		open_on_weekends,
+	});
+
+	await orphanagesRepository.save(orphanage);
+
 	return response.json({message: 'Hello world'});
 });
